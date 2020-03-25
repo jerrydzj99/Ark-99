e80c08ec04915a8a97deb7f98efe459b5fb3f934
Jerry Ding <jerryding2022@berkeley.edu>
2020-03-25 14:04:42 +0800

    Set up player movement and camera follow

---
+++ Ark 99/Assets/Scripts/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPosition = player.transform.position;
+        Vector3 cameraPosition = new Vector3(0f + playerPosition.x, 10f + playerPosition.y, -4f + playerPosition.z);
+        Quaternion cameraRotation = Quaternion.identity;
+        cameraRotation.eulerAngles = new Vector3(67f, 0f, 0f);
+        transform.position = cameraPosition;
+        transform.rotation = cameraRotation;
+    }
+}
---
+++ Ark 99/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float maxSpeed;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Keyboard-controlled movement
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0f, 0f, 5000 * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0f, 0f, -5000 * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(5000 * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-5000 * Time.deltaTime, 0f, 0f);
+        }
+
+        // Maximum velocity enforcement
+        if (Vector3.Magnitude(rb.velocity) > maxSpeed)
+        {
+            Vector3 newVelocity = maxSpeed * rb.velocity.normalized;
+            rb.velocity = newVelocity;
+        }
+    }
+}
